4e2e4001a51c4ec0e6c056376851dcb8768ad8b6
Fernando C <fernandoeqc@live.com>
2020-10-17 17:21:52 -0300

    criado tela tkinter p entrada de dados

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-import pyHook
-import pythoncom
-
-def tecla_pressionada(evento):
-   # print("Alguma tecla pressionada")
-    print (chr(evento.Ascii))
-
-hook = pyHook.HookManager()
-hook.KeyDown = tecla_pressionada #sem ()
-hook.HookKeyboard()
-
-pythoncom.PumpMessages() #cria loop infinito e espera mensagens do SO
+label1
--- unit_test.py
+++ unit_test.py
@@ -5,7 +5,7 @@ import tkinter as tk
 """ arduino_uno = serial.Serial('COM1', 9600)
 print("criou serial")
 
-#arduino_uno.open()  
+#arduino_uno.open()
 #print("abriu serial")
 
 ligar_led = 's'
@@ -42,8 +42,46 @@ def keypress(event):
 
 
 root = tk.Tk()
-print("Press a key (Escape key to exit):")
+
+canvas1 = tk.Canvas(root, width=400, height=300)
+canvas1.pack()
+
+entry1 = tk.Entry(root)
+canvas1.create_window(200, 140, window=entry1)
+
+label1 = None
+
+
+def delButton():
+    label1.destroy()
+
+
+def getSquareRoot():
+    global label1
+
+    if label1 is not None:
+        label1.destroy()
+        print("destruido")
+
+    x1 = entry1.get()
+
+    label1 = tk.Label(root, text=x1)
+    label1.pack(pady=10)
+    print(label1)
+    #canvas1.create_window(200, 230, window=label1)
+
+
+send_button = tk.Button(text='Get the Square Root', command=getSquareRoot)
+canvas1.create_window(200, 180, window=send_button)
+
+delete_button = tk.Button(text='Delete the label', command=delButton)
+canvas1.create_window(200, 210, window=delete_button)
+
+
+
+
+""" print("Press a key (Escape key to exit):")
 root.bind_all('<Key>', keypress)
 # don't show the tk window
-root.withdraw()
+root.withdraw() """
 root.mainloop()
